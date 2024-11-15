bbf06ef6445f199850cf2425511d46ace06e1682
Enrico Caliolo <enricocalioloo@gmail.com>
2024-11-15 16:07:23 +0100

    new db design

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -4,40 +4,30 @@ CREATE DATABASE wasa;
 
 USE wasa;
 
-CREATE TABLE User (
+CREATE TABLE `User` (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(64) NOT NULL,
     icon VARCHAR(64),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Conversation (
+CREATE TABLE `Conversation` (
     conversation_id INT AUTO_INCREMENT PRIMARY KEY,
-    user1 INT,
-    user2 INT,
-    FOREIGN KEY (user1) REFERENCES User(user_id),
-    FOREIGN KEY (user2) REFERENCES User(user_id)
+    `name` VARCHAR(64),
+    is_group BOOLEAN,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `Group` (
-    group_id INT AUTO_INCREMENT PRIMARY KEY,
-    group_name VARCHAR(64) NOT NULL,
-    icon BLOB,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    created_by_user_id INT,
-    FOREIGN KEY (created_by_user_id) REFERENCES User (user_id)
-);
-
-CREATE TABLE GroupParticipants (
-    group_id INT,
+CREATE TABLE `ConversationParticipants` (
+    conversation_id INT,
     user_id INT,
     joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (group_id, user_id),
-    FOREIGN KEY (group_id) REFERENCES `Group`(group_id),
-    FOREIGN KEY (user_id) REFERENCES User(user_id)
+    PRIMARY KEY (conversation_id, user_id),
+    FOREIGN KEY (conversation_id) REFERENCES `Conversation`(conversation_id),
+    FOREIGN KEY (user_id) REFERENCES `User`(user_id)
 );
 
-CREATE TABLE Message (
+CREATE TABLE `Message` (
     message_id INT AUTO_INCREMENT PRIMARY KEY,
     content BLOB NOT NULL,
     sent_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -48,11 +38,10 @@ CREATE TABLE Message (
     group_id INT,
     replied_to INT,
     forwarded_from INT,
-    FOREIGN KEY (sender_id) REFERENCES User(user_id),
-    FOREIGN KEY (conversation_id) REFERENCES Conversation(conversation_id),
-    FOREIGN KEY (group_id) REFERENCES `Group`(group_id),
+    FOREIGN KEY (sender_id) REFERENCES `User`(user_id),
+    FOREIGN KEY (conversation_id) REFERENCES `Conversation`(conversation_id),
     FOREIGN KEY (replied_to) REFERENCES `Message`(message_id),
-    FOREIGN KEY (forwarded_from) REFERENCES `Group`(message_id),
+    FOREIGN KEY (forwarded_from) REFERENCES `Message`(message_id)
 );
 
 -- users
@@ -62,18 +51,19 @@ VALUES  (1, 'enrico'),
         (2, 'manu'),
         (3, 'teste');
 
-INSERT INTO Conversation (conversation_id, user1, user2)
-VALUES (1, 1, 2);
+INSERT INTO `Conversation` (conversation_id)
+VALUES  (1),
+        (2);
 
-INSERT INTO `GROUP` (group_id, group_name)
-VALUES (1, 'grupo');
 
-INSERT INTO GroupParticipants (group_id, user_id)
+INSERT INTO `ConversationParticipants` (conversation_id, user_id)
 VALUES  (1, 1),
         (1, 2),
-        (1, 3);
+        (1, 3),
+        (2, 1),
+        (2, 3);
 
-INSERT INTO Message (id_message, content, sender_id, conversation_id)
+INSERT INTO `Message` (message_id, content, sender_id, conversation_id)
 VALUES  (1, 'oi, tudo bem?', 1, 1),
         (2, 'tudo bem, e vc?', 2, 1),
         (3, 'tudo bem tbm', 1, 1);
