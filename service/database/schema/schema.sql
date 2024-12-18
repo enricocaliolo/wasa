f542c74168d90e16ee69de4492dd0a9735700231
Enrico Caliolo <enricocalioloo@gmail.com>
2024-12-18 19:50:15 +0100

    inserted more mock data

--- service/database/schema/schema.sql
+++ service/database/schema/schema.sql
@@ -52,27 +52,3 @@ CREATE TABLE `Reactions` (
     FOREIGN KEY (message_id) REFERENCES `Message`(message_id),
     FOREIGN KEY (user_id) REFERENCES `User`(user_id)
 );
-
--- users
-
-INSERT INTO User (user_id, username)
-VALUES  (1, 'enrico'),
-        (2, 'manu'),
-        (3, 'teste');
-
-INSERT INTO `Conversation` (conversation_id)
-VALUES  (1),
-        (2);
-
-
-INSERT INTO `ConversationParticipants` (conversation_id, user_id)
-VALUES  (1, 1),
-        (1, 2),
-        (1, 3),
-        (2, 1),
-        (2, 3);
-
-INSERT INTO `Message` (message_id, content, sender_id, conversation_id)
-VALUES  (1, 'oi, tudo bem?', 1, 1),
-        (2, 'tudo bem, e vc?', 2, 1),
-        (3, 'tudo bem tbm', 1, 1);
